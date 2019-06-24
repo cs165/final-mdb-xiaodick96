5dfd5b36e020fe28a52881c1a95d68b886f6401f
xiaodick96 <xiaodick96@hotmail.com>
2019-06-24 15:46:22 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+
+const api = require('./routes/api.js');
+const views = require('./routes/views.js');
+
+const app = express();
+const jsonParser = bodyParser.json();
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
 
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const app = express();
-const jsonParser = bodyParser.json();
-
-app.use(express.static('public'));
-
-let db = null;
 async function main() {
   const DATABASE_NAME = 'cs193x-db';
-  const MONGO_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
+  const MONGO_URL = `mongodb+srv://404410007:<password>@final-project-ex1lc.mongodb.net/test?retryWrites=true&w=majority`;
+  
+  let cli = await MongoClient.connect(process.env.MONGODB_URI || MONGO_URL);
+  let db = cli.db(DATABASE_NAME);
 
-  // The "process.env.MONGODB_URI" is needed to work with Heroku.
-  db = await MongoClient.connect(process.env.MONGODB_URI || MONGO_URL);
+  const diaries = db.collection('diaries');
+  const entries = db.collection('entries');
 
-  // The "process.env.PORT" is needed to work with Heroku.
+  function setCollection(req, res, next) {
+    req.diaries = diaries;
+    req.entries = entries;
+    next();
+  }
+  app.use(setCollection);
+  app.use(api);
+  app.use(views);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server listening on port ${port}!`);
 };
 
 main();
-
-////////////////////////////////////////////////////////////////////////////////
-
-// TODO(you): Add at least 1 GET route and 1 POST route.
